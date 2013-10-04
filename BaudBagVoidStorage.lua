6d8b1793fad387d6f502e3053d5acb930eeef2af
Michalis Adamidis <gsnerf@gsnerf.de>
2013-10-05 00:08:15 +0200

    further experiments with original void storage, events and first steps to hooking into the system

--- BaudBagVoidStorage.lua
+++ BaudBagVoidStorage.lua
@@ -7,13 +7,67 @@ function BaudBagToggleVoidStorage(self)
 
 	-- toggle switch
 	if (not VoidStorageFrame:IsShown()) then
-		BaudBag_DebugMsg("VoidStorage", "... not yet visible, updating and showing! ("..CanUseVoidStorage().."|"..tostring(IsVoidStorageReady())..")");
+		BaudBag_DebugMsg("VoidStorage", "... not yet visible, updating and showing! ("..tostring(CanUseVoidStorage()).."|"..tostring(IsVoidStorageReady())..")");
 		-- before showing we need to make sure the items where initialized
 		-- else the void storage will show up empty
 		VoidStorageFrame_Update();
 		VoidStorageFrame_Show();
+
+		for i = 1, 80 do
+			local itemID, textureName, locked, recentDeposit, isFiltered = GetVoidItemInfo(i);
+			if (itemID) then
+				BaudBag_DebugMsg("VoidStorage", "found item with id: "..itemID);
+			end
+		end
 	else
 		BaudBag_DebugMsg("VoidStorage", "... already visible, hiding!");
 		VoidStorageFrame_Hide();
 	end
+end
+
+--[[ Event functions that will be called from the frame script functions later ]]
+local EventFuncs = {}
+
+
+local eventFunc = function(self, event, ...)
+	BaudBag_DebugMsg("VoidStorage", "Handling event "..event);
+end
+EventFuncs.VOID_STORAGE_OPEN = eventFunc;
+EventFuncs.VOID_STORAGE_CONTENTS_UPDATE = eventFunc;
+
+--EventFuncs.VOID_STORAGE_OPEN = Func;
+--EventFuncs.VOID_DEPOSIT_WARNING = Func;
+--EventFuncs.VOID_STORAGE_DEPOSIT_UPDATE = Func;
+--EventFuncs.VOID_TRANSFER_DONE = Func;
+--EventFuncs.VOID_STORAGE_CONTENTS_UPDATE = Func;
+--EventFuncs.VOID_STORAGE_UPDATE = Func;
+--EventFuncs.VOID_STORAGE_CLOSE = Func;
+
+
+--[[                  Frame script functions                        ]]
+--[[ -------------------------------------------------------------- ]]
+function BaudBagVoidStorage_OnLoad(self, event, ...)
+	-- make sure all needed events are registered to this frame
+	for Key, Value in pairs(EventFuncs) do
+		self:RegisterEvent(Key);
+	end
+end
+
+function BaudBagVoidStorage_OnEvent(self, event, ...)
+	EventFuncs[event](self, event, ...);
+end
+
+--[[               PreHooks for original void storage               ]]
+--[[ -------------------------------------------------------------- ]]
+
+local origVoidStorageFrame_Show = VoidStorageFrame_Show;
+VoidStorageFrame_Show = function(...)
+	BaudBag_DebugMsg("VoidStorage", "Tried to catch the Show event");
+	return origVoidStorageFrame_Show(...);
+end
+
+local origVoidStorageFrame_Hide = VoidStorageFrame_Hide;
+VoidStorageFrame_Hide = function(...)
+	BaudBag_DebugMsg("VoidStorage", "Tried to catch the Hide event");
+	return origVoidStorageFrame_Hide(...);
 end
