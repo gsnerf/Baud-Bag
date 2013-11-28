4520b83d01acdd759ca534cda8ab6be3a509a1b4
Michalis Adamidis <gsnerf@gsnerf.de>
2013-11-29 00:18:13 +0100

    extended DebugMsg with possibility to dump vars directly into the chat frame instead of having to create those kind of outputs in the debug message

--- BaudBagFunctions.lua
+++ BaudBagFunctions.lua
@@ -39,9 +39,51 @@ local BaudBag_DebugCfg = {
 
 
 
-function BaudBag_DebugMsg(type, msg)
+function BaudBag_DebugMsg(type, msg, ...)
 	if (BaudBag_DebugCfg[type].Active) then
 		DEFAULT_CHAT_FRAME:AddMessage(GetTime().." BaudBag ("..BaudBag_DebugCfg[type].Name.."): "..msg);
+		if (... ~= nil) then
+			BaudBag_Vardump(...)
+		end
+	end
+end
+
+function BaudBag_Vardump(value, depth, key)
+	local linePrefix = "";
+	local spaces = "";
+  
+	if key ~= nil then
+		linePrefix = "["..key.."] = ";
+	end
+  
+	if depth == nil then
+		depth = 0;
+	else
+		depth = depth + 1;
+		for i=1, depth do
+			spaces = spaces .. "  "; 
+		end
+	end
+  
+	if type(value) == 'table' then
+		mTable = getmetatable(value);
+		if mTable == nil then
+			DEFAULT_CHAT_FRAME:AddMessage(GetTime().." BaudBag (vardump): "..spaces..linePrefix.."(table) ");
+		else
+			DEFAULT_CHAT_FRAME:AddMessage(GetTime().." BaudBag (vardump): "..spaces.."(metatable) ");
+			value = mTable;
+		end
+		for tableKey, tableValue in pairs(value) do
+			vardump(tableValue, depth, tableKey);
+		end
+	elseif type(value)	== 'function' or 
+		type(value)	== 'thread' or 
+		type(value)	== 'userdata' or
+		value		== nil
+	then
+		DEFAULT_CHAT_FRAME:AddMessage(GetTime().." BaudBag (vardump): "..spaces..tostring(value));
+	else
+		DEFAULT_CHAT_FRAME:AddMessage(GetTime().." BaudBag (vardump): "..spaces..linePrefix.."("..type(value)..") "..tostring(value));
 	end
 end
 
