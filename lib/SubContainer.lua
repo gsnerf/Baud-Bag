c30977585d99b63d129dc27e26b9607b7a6ffc2d
gsnerf <gsnerf@gsnerf.de>
2017-04-27 09:25:52 +0200

    reworked subcontainer to use object oriented approach and first extensions to also create a frame belonging to the object

--- lib/SubContainer.lua
+++ lib/SubContainer.lua
@@ -1,11 +1,28 @@
-SubContainer = {
+local AddOnName, AddOnTable = ...
+local _
+
+local Prototype = {
     startColumn = 0,
-    highlighted = false
+    highlighted = false,
+    frame = nil
 }
 
-function SubContainer:toggleHighlight()
-    highlighted = not highlighted
+function Prototype:ToggleHighlight()
+    self.highlighted = not self.highlighted
 end
 
-function SubContainer:render()
+function Prototype:GetFrame()
+    return self.frame
+end
+
+function Prototype:Render()
+
+end
+
+local Metatable = { __index = Prototype }
+
+function AddOnTable:CreateSubContainer(subBagTemplate)
+    local subContainer = _G.setmetatable({}, Metatable)
+    subContainer.frame = CreateFrame("Frame", AddOnName.."SubBag"..Bag, nil, subBagTemplate);
+    return subContainer
 end
