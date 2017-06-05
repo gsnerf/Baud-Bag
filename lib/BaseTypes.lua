f09d34e245c1ce5d93d1b662b66d35c71c60d1f4
gsnerf <gsnerf@gsnerf.de>
2017-06-05 17:42:36 +0200

    Reworked BaseTypes to enum like usage.

--- lib/BaseTypes.lua
+++ lib/BaseTypes.lua
@@ -1,8 +1,30 @@
 BagSetType = {
-    Backpack = 1,
-    Bank = 2,
-    GuildBank = 3,
-    VoidStorage = 4
+    Backpack = {
+        Id = 1,
+        IsSubContainerOf = function(containerId)
+            return (BACKPACK_CONTAINER <= containerId  and containerId <= BACKPACK_CONTAINER + NUM_BAG_SLOTS)
+        end
+    },
+    Bank = {
+        Id = 2,
+        IsSubContainerOf = function(containerId)
+            local isBankDefaultContainer = (containerId == BANK_CONTAINER) or (containerId == REAGENTBANK_CONTAINER)
+            local isBankSubContainer = (ITEM_INVENTORY_BANK_BAG_OFFSET < containerId) and (containerId <= ITEM_INVENTORY_BANK_BAG_OFFSET + NUM_BANKBAGSLOTS)
+            return isBankDefaultContainer or isBankSubContainer
+        end
+    } --[[,
+    GuildBank = {
+        Id = 3,
+        IsSubContainerOf = function(containerId)
+            return false
+        end
+    },
+    VoidStorage = {
+        Id = 4,
+        IsSubContainerOf = function(containerId)
+            return false
+        end
+    } ]]
 }
 
 ContainerType = {
