83ccadd97690b6c6d0c7b35f64e63b33281f906a
gsnerf <gsnerf@gsnerf.de>
2017-06-18 20:06:05 +0200

    Reworked formatting of new TextureHelper and added optional parameters to overwrite Width/Height instead of dynamically calculating them.

--- lib/TextureHelper.lua
+++ lib/TextureHelper.lua
@@ -8,20 +8,37 @@ local Prototype = {
     Height
 }
 
-function Prototype:GetTexturePiece(Name, MinX, MaxX, MinY, MaxY, Layer)
-    local Texture = _G[self.Parent:GetName()..Name];
+--- Creates a new Texture inside the current value of Parent.
+-- The image is read from the currently set File which has the currently set Width and Height.
+-- Those values are needed to calculate the correct relative values for left/right/top/bottom.
+-- @param Name Value to use for creating the new texture
+-- @param MinX The left most pixel of the texture image
+-- @param MaxX The right most pixel of the texture image
+-- @param MinY The top most pixel of the texture image
+-- @param MaxY The bottom most pixel of the texture image
+-- @param[opt=MaxX - MinX + 1] Width The target width
+-- @param[opt=MaxY - MinY + 1] Height The target height
+-- @param[opt="BACKGROUND"] Layer The layer to write the texture to
+function Prototype:GetTexturePiece(Name, MinX, MaxX, MinY, MaxY, Width, Height, Layer)
+    -- handle optional parameters
+    Layer = Layer or "BACKGROUND"
+    Width = Width or (MaxX - MinX + 1)
+    Height = Height or (MaxY - MinY + 1)
+
+    local Texture = _G[self.Parent:GetName()..Name]
     if not Texture then
-        Texture = self.Parent:CreateTexture(self.Parent:GetName()..Name);
+        Texture = self.Parent:CreateTexture(self.Parent:GetName()..Name)
     end
-    Texture:ClearAllPoints();
-    Texture:SetTexture(self.File);
-    Texture:SetTexCoord(MinX / self.Width, (MaxX + 1) / self.Width, MinY / self.Height, (MaxY + 1) / self.Height);
-    Texture:SetWidth(MaxX - MinX + 1);
-    Texture:SetHeight(MaxY - MinY + 1);
-    Texture:SetDrawLayer(Layer);
-    Texture:Show();
-    --  Texture:SetVertexColor(0.2,0.2,1);
-    return Texture;
+
+    Texture:ClearAllPoints()
+    Texture:SetTexture(self.File)
+    Texture:SetTexCoord(MinX / self.Width, (MaxX + 1) / self.Width, MinY / self.Height, (MaxY + 1) / self.Height)
+    Texture:SetWidth(Width)
+    Texture:SetHeight(Height)
+    Texture:SetDrawLayer(Layer)
+    Texture:Show()
+    
+    return Texture
 end
 
 local Metatable = { __index = Prototype }
