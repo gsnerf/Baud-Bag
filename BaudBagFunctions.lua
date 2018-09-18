d938b95146d958a72093efe578a607420e1852fc
gsnerf <gsnerf@gsnerf.de>
2018-09-18 21:12:22 +0200

    Deactivated debug output for cache again.

--- BaudBagFunctions.lua
+++ BaudBagFunctions.lua
@@ -18,7 +18,7 @@ local BaudBag_DebugCfg = {
     MenuDropDown   = { Name = "Menu DropDown",      Active = false },
 	
     -- everything that has to do with offline capabilities
-    Cache       = { Name = "Cache",         Active = true },
+    Cache       = { Name = "Cache",         Active = false },
     Bank        = { Name = "Bank",          Active = false },
     BankReagent = { Name = "Reagent Bank",  Active = false },
     VoidStorage = { Name = "Void Storage",  Active = false },
