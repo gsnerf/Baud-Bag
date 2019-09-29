630410e4bc3392bdcbd787fee809b5eb16df03be
gsnerf <gsnerf@gsnerf.de>
2019-09-29 21:42:16 +0200

    Fixed background for money frames

--- lib/Backgrounds/BlizzardBackground.lua
+++ lib/Backgrounds/BlizzardBackground.lua
@@ -199,17 +199,17 @@ function Prototype:RenderMoneyFrameBackground(helper, containerFrame, parentName
     local moneyFrameName = containerFrame.MoneyFrame:GetName()
 
     -- left part of ONLY the yellow border
-    local texture = helper:GetTexturePiece("MoneyLineLeft", 80,84, 228,246, nil, targetHeight)
+    local texture = helper:GetTexturePiece("MoneyLineLeft", 80,84, 213,231, nil, targetHeight)
     texture:SetPoint("LEFT", parentName.."Left", "RIGHT")
     texture:SetPoint("TOP", moneyFrameName, "TOP", 0, 0)
 
     -- right part of ONLY the yellow border
-    texture = helper:GetTexturePiece("MoneyLineRight", 240,244, 228,246, nil, targetHeight)
+    texture = helper:GetTexturePiece("MoneyLineRight", 240,244, 213,231, nil, targetHeight)
     texture:SetPoint("RIGHT", parentName.."Right", "LEFT")
     texture:SetPoint("TOP", moneyFrameName, "TOP", 0, 0)
 
     -- center part of ONLY the yellow border
-    texture = helper:GetTexturePiece("MoneyLineCenter", 85,239, 228,246, nil, targetHeight)
+    texture = helper:GetTexturePiece("MoneyLineCenter", 85,239, 213,231, nil, targetHeight)
     texture:SetPoint("LEFT", parentName.."MoneyLineLeft", "RIGHT")
     texture:SetPoint("RIGHT", parentName.."MoneyLineRight", "LEFT")
 end
