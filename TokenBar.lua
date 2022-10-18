75d7f4054b1313b6f49273b950b44395ba112b3b
gsnerf <gsnerf@gsnerf.de>
2022-10-18 17:58:29 +0200

    Replaced all debug messages calls in TokenFrame with new method references in addon table.

--- TokenBar.lua
+++ TokenBar.lua
@@ -18,18 +18,18 @@ MAX_WATCHED_TOKENS_BAUD_BAG = 5
 local function calculateScaleFix(TokenFrame)
     local scale = TokenFrame:GetEffectiveScale()
     local scaleFix = 5 - math.ceil(scale / 0.2)
-    BaudBag_DebugMsg("Token", "Applying scale fix (scale, scaleFix)", scale, scaleFix)
+    AddOnTable.Functions.DebugMessage("Token", "Applying scale fix (scale, scaleFix)", scale, scaleFix)
     return scaleFix
 end
 
 local TokenFrame_Update = function()
-    BaudBag_DebugMsg("Token", "Update was called on TokenFrame")
+    AddOnTable.Functions.DebugMessage("Token", "Update was called on TokenFrame")
     -- make sure the old is called when BaudBag is disabled for the backpack
     if (BBConfig and BBConfig[1].Enabled == false) then
-        --BaudBag_DebugMsg("Token", "BaudBag disabled for Backpack, calling original!")
+        --AddOnTable.Functions.DebugMessage("Token", "BaudBag disabled for Backpack, calling original!")
         --MAX_WATCHED_TOKENS = MAX_WATCHED_TOKENS_ORIG
         --return pre_BackpackTokenFrame_Update()
-        BaudBag_DebugMsg("Token", "BaudBag disabled for Backpack, skipping!")
+        AddOnTable.Functions.DebugMessage("Token", "BaudBag disabled for Backpack, skipping!")
         return
     end
 	
@@ -50,7 +50,7 @@ local TokenFrame_Update = function()
 
         -- Update watched tokens
         if ( currencyInfo ) then
-            BaudBag_DebugMsg("Token", "Update: Token "..i.." found")
+            AddOnTable.Functions.DebugMessage("Token", "Update: Token "..i.." found")
 			
             -- set icon
             watchButton.Icon:SetTexture(currencyInfo.iconFileID)
@@ -64,7 +64,7 @@ local TokenFrame_Update = function()
 			
             -- update width based on text to show
             digits = string.len(tostring(currencyInfo.quantity))
-            BaudBag_DebugMsg("Token", "number of digits in currency '"..currencyInfo.name.."': "..digits)
+            AddOnTable.Functions.DebugMessage("Token", "number of digits in currency '"..currencyInfo.name.."': "..digits)
             countSize = digits * digitWidth + math.floor(6 / digits)
             watchButton.Quantity:SetWidth(countSize)
             -- 12 (icon width) + 1 (space between quantity & icon) + quantity width + 5 (space to the left)
@@ -72,7 +72,7 @@ local TokenFrame_Update = function()
 
             -- recalc used width now
             usedWidth = usedWidth + watchButton:GetWidth()
-            BaudBag_DebugMsg("Token", "used width after currency '"..currencyInfo.name.."': "..usedWidth.." space available: "..TokenFrame:GetWidth())
+            AddOnTable.Functions.DebugMessage("Token", "used width after currency '"..currencyInfo.name.."': "..usedWidth.." space available: "..TokenFrame:GetWidth())
 			
             -- make only visible if there is enough space 
             watchButton:Hide()
@@ -84,12 +84,12 @@ local TokenFrame_Update = function()
                 TokenFrame.numWatchedTokens = i
             end
         else
-            BaudBag_DebugMsg("Token", "Update: Token "..i.." NOT found")
+            AddOnTable.Functions.DebugMessage("Token", "Update: Token "..i.." NOT found")
             watchButton:Hide()
             watchButton.currencyType = nil
             watchButton.amount = 0
             if ( i == 1 ) then
-                BaudBag_DebugMsg("Token", "Update: Token 1 => hiding backpack")
+                AddOnTable.Functions.DebugMessage("Token", "Update: Token 1 => hiding backpack")
                 TokenFrame.shouldShow = 0
             end
         end
@@ -102,7 +102,7 @@ local pre_GetNumWatchedTokens = GetNumWatchedTokens
 GetNumWatchedTokens = function()
     -- make sure the old is called when baudbag is disabled for the bagpack
     if (BBConfig and BBConfig[1].Enabled == false) then
-        BaudBag_DebugMsg("Token", "BaudBag disabled for Backpack, calling original!")
+        AddOnTable.Functions.DebugMessage("Token", "BaudBag disabled for Backpack, calling original!")
         return pre_GetNumWatchedTokens()
     end
 	
@@ -117,10 +117,10 @@ end
 
 local pre_BackpackTokenFrame_IsShown = BackpackTokenFrame_IsShown
 BackpackTokenFrame_IsShown = function()
-    BaudBag_DebugMsg("Token", "IsShown was called on BaudBagTokenFrame")
+    AddOnTable.Functions.DebugMessage("Token", "IsShown was called on BaudBagTokenFrame")
     -- make sure the old is called when BaudBag is disabled for the backpack
     if (BBConfig and BBConfig[1].Enabled == false) then
-        BaudBag_DebugMsg("Token", "BaudBag disabled for Backpack, calling original!")
+        AddOnTable.Functions.DebugMessage("Token", "BaudBag disabled for Backpack, calling original!")
         return pre_BackpackTokenFrame_IsShown()
     end
 
@@ -129,9 +129,9 @@ end
 
 
 local ManageTokenFrame = function(backpack)
-    BaudBag_DebugMsg("Token", "Manage was called on TokenFrame")
+    AddOnTable.Functions.DebugMessage("Token", "Manage was called on TokenFrame")
     if (BBConfig and BBConfig[1].Enabled == false) then
-        BaudBag_DebugMsg("Token", "BaudBag disabled for Backpack, ignoring!")
+        AddOnTable.Functions.DebugMessage("Token", "BaudBag disabled for Backpack, ignoring!")
         return
     end
 	
@@ -140,11 +140,11 @@ local ManageTokenFrame = function(backpack)
     local Backpack   = _G["BaudBagContainer1_1"]
 
     if (BackpackTokenFrame_IsShown() == 1) and (not TokenFrame:IsShown()) then
-        BaudBag_DebugMsg("Token", "Manage: TokenFrame visible, update settings")
+        AddOnTable.Functions.DebugMessage("Token", "Manage: TokenFrame visible, update settings")
         TokenFrame:Show()
         BaudBagUpdateContainer(Backpack)
     elseif (BackpackTokenFrame_IsShown() ~= 1 and TokenFrame:IsShown()) then
-        BaudBag_DebugMsg("Token", "Manage: TokenFrame NOT visible, hide it")
+        AddOnTable.Functions.DebugMessage("Token", "Manage: TokenFrame NOT visible, hide it")
         TokenFrame:Hide() 
         BaudBagUpdateContainer(Backpack)
     end
@@ -173,7 +173,7 @@ BaudBagTokenFrameMixin = {}
 
 function BaudBagTokenFrameMixin:RenderBackground(texturesParentName)
     local container = self:GetParent()
-    BaudBag_DebugMsg("Token", "Showing Token Frame for Container (Name, ID)'", container:GetName(), container:GetID())
+    AddOnTable.Functions.DebugMessage("Token", "Showing Token Frame for Container (Name, ID)'", container:GetName(), container:GetID())
     
     -- init texture helper
     local helper = AddOnTable:GetTextureHelper()
