c93f9858f64ed756301bd8eab1523f22ca996796
gsnerf <gsnerf@gsnerf.de>
2022-10-23 22:32:44 +0200

    Hopefully found a way to switch between DF and SL API without multiple files and toc changes.

--- API.lua
+++ API.lua
@@ -1,56 +1,91 @@
 local _
 local AddOnName, AddOnTable = ...
 
-AddOnTable.BlizzAPI = {
-    ContainerIDToInventoryID = ContainerIDToInventoryID,
-    GetContainerNumSlots = GetContainerNumSlots,
-    GetContainerNumFreeSlots = GetContainerNumFreeSlots,
-    GetContainerFreeSlots = GetContainerFreeSlots,
-    GetInventorySlotInfo = GetInventorySlotInfo,
-    ---wrapper for the regular GetContainerItemInfo so we can support the addon in multiple interface levels
-    ---@param containerId number ID of the container the item is located in
-    ---@param slotIndex number ID of the slot in the container the item is located in
-    ---@return { iconFileID: number, stackCount: number, isLocked: boolean, quality: Enum.ItemQuality?, isReadable: boolean, hasLoot: boolean, hyperlink: string, isFiltered: boolean, hasNoValue: boolean, itemID: number, isBound: boolean }|nil
-    GetContainerItemInfo = function(containerId, slotIndex)
-        local texture, count, locked, quality, isReadable, lootable, link, isFiltered, hasNoValue, itemID, isBound = GetContainerItemInfo(containerId, slotIndex)
-        return {
-            iconFileID = texture,
-            stackCount = count,
-            isLocked = locked,
-            quality = quality,
-            isReadable = isReadable,
-            hasLoot = lootable,
-            hyperlink = link,
-            isFiltered = isFiltered,
-            hasNoValue = hasNoValue,
-            itemID = itemID,
-            isBound = isBound,
-        }
-    end,
-    --- returns information about quest related information from an item
-    ---@param containerID number ID of the container 
-    ---@param slotIndex number index of the slot in the container
-    ---@return {isQuestItem: boolean, questID: number?, isActive: boolean }
-    GetContainerItemQuestInfo = function(containerID, slotIndex)
-        local isQuestItem, questID, isActive = GetContainerItemQuestInfo(containerID, slotIndex)
-        return {
-            isQuestItem = isQuestItem,
-            questID = questID,
-            isActive = isActive
-        }
-    end,
-    GetBackpackAutosortDisabled = GetBackpackAutosortDisabled,
-    GetBankAutosortDisabled = GetBankAutosortDisabled,
-    GetContainerItemID = GetContainerItemID,
-    GetContainerItemLink = GetContainerItemLink,
-    IsBattlePayItem = IsBattlePayItem,
-    PickupContainerItem = PickupContainerItem,
-    SetBackpackAutosortDisabled = SetBackpackAutosortDisabled,
-    SetBankAutosortDisabled = SetBankAutosortDisabled,
-    SortBankBags = SortBankBags,
-    SortReagentBankBags = SortReagentBankBags,
-    SplitContainerItem = SplitContainerItem,
-    UseContainerItem = UseContainerItem,
-    IsNewItem = C_NewItems.IsNewItem,
-    GetItemInfo = GetItemInfo
-}
+if C_Container ~= nil then
+
+    AddOnTable.BlizzAPI = {
+        ContainerIDToInventoryID = C_Container.ContainerIDToInventoryID,
+        GetContainerNumSlots = C_Container.GetContainerNumSlots,
+        GetContainerNumFreeSlots = C_Container.GetContainerNumFreeSlots,
+        GetContainerFreeSlots = C_Container.GetContainerFreeSlots,
+        GetInventorySlotInfo = GetInventorySlotInfo,
+        ---wrapper for the regular GetContainerItemInfo so we can support the addon in multiple interface levels
+        ---@param containerId number ID of the container the item is located in
+        ---@param slotIndex number ID of the slot in the container the item is located in
+        ---@return { iconFileID: number, stackCount: number, isLocked: boolean, quality: Enum.ItemQuality?, isReadable: boolean, hasLoot: boolean, hyperlink: string, isFiltered: boolean, hasNoValue: boolean, itemID: number, isBound: boolean }|nil
+        GetContainerItemInfo = C_Container.GetContainerItemInfo,
+        GetContainerItemQuestInfo = C_Container.GetContainerItemQuestInfo,
+        GetBackpackAutosortDisabled = C_Container.GetBackpackAutosortDisabled,
+        GetBankAutosortDisabled = C_Container.GetBankAutosortDisabled,
+        GetContainerItemID = C_Container.GetContainerItemID,
+        GetContainerItemLink = C_Container.GetContainerItemLink,
+        IsBattlePayItem = C_Container.IsBattlePayItem,
+        PickupContainerItem = C_Container.PickupContainerItem,
+        SetBackpackAutosortDisabled = C_Container.SetBackpackAutosortDisabled,
+        SetBankAutosortDisabled = C_Container.SetBankAutosortDisabled,
+        SortBags = C_Container.SortBags,
+        SortBankBags = C_Container.SortBankBags,
+        SortReagentBankBags = C_Container.SortReagentBankBags,
+        SplitContainerItem = C_Container.SplitContainerItem,
+        UseContainerItem = C_Container.UseContainerItem,
+        IsNewItem = C_NewItems.IsNewItem,
+        GetItemInfo = GetItemInfo
+    }
+
+else
+
+    AddOnTable.BlizzAPI = {
+        ContainerIDToInventoryID = ContainerIDToInventoryID,
+        GetContainerNumSlots = GetContainerNumSlots,
+        GetContainerNumFreeSlots = GetContainerNumFreeSlots,
+        GetContainerFreeSlots = GetContainerFreeSlots,
+        GetInventorySlotInfo = GetInventorySlotInfo,
+        ---wrapper for the regular GetContainerItemInfo so we can support the addon in multiple interface levels
+        ---@param containerId number ID of the container the item is located in
+        ---@param slotIndex number ID of the slot in the container the item is located in
+        ---@return { iconFileID: number, stackCount: number, isLocked: boolean, quality: Enum.ItemQuality?, isReadable: boolean, hasLoot: boolean, hyperlink: string, isFiltered: boolean, hasNoValue: boolean, itemID: number, isBound: boolean }|nil
+        GetContainerItemInfo = function(containerId, slotIndex)
+            local texture, count, locked, quality, isReadable, lootable, link, isFiltered, hasNoValue, itemID, isBound = GetContainerItemInfo(containerId, slotIndex)
+            return {
+                iconFileID = texture,
+                stackCount = count,
+                isLocked = locked,
+                quality = quality,
+                isReadable = isReadable,
+                hasLoot = lootable,
+                hyperlink = link,
+                isFiltered = isFiltered,
+                hasNoValue = hasNoValue,
+                itemID = itemID,
+                isBound = isBound,
+            }
+        end,
+        --- returns information about quest related information from an item
+        ---@param containerID number ID of the container 
+        ---@param slotIndex number index of the slot in the container
+        ---@return {isQuestItem: boolean, questID: number?, isActive: boolean }
+        GetContainerItemQuestInfo = function(containerID, slotIndex)
+            local isQuestItem, questID, isActive = GetContainerItemQuestInfo(containerID, slotIndex)
+            return {
+                isQuestItem = isQuestItem,
+                questID = questID,
+                isActive = isActive
+            }
+        end,
+        GetBackpackAutosortDisabled = GetBackpackAutosortDisabled,
+        GetBankAutosortDisabled = GetBankAutosortDisabled,
+        GetContainerItemID = GetContainerItemID,
+        GetContainerItemLink = GetContainerItemLink,
+        IsBattlePayItem = IsBattlePayItem,
+        PickupContainerItem = PickupContainerItem,
+        SetBackpackAutosortDisabled = SetBackpackAutosortDisabled,
+        SetBankAutosortDisabled = SetBankAutosortDisabled,
+        SortBankBags = SortBankBags,
+        SortReagentBankBags = SortReagentBankBags,
+        SplitContainerItem = SplitContainerItem,
+        UseContainerItem = UseContainerItem,
+        IsNewItem = C_NewItems.IsNewItem,
+        GetItemInfo = GetItemInfo
+    }
+
+end
