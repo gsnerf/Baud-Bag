68b44c1e619df594b844a00e74f99fdbc2c6b45b
gsnerf <gsnerf@gsnerf.de>
2025-08-11 23:13:32 +0200

    Reworked how borders in ItemButton are defined to reduce effect of external item button updates replacing the border texture.

--- widgets/buttons/ItemButton.lua
+++ widgets/buttons/ItemButton.lua
@@ -337,15 +337,18 @@ function Prototype:ShowHighlight(enabled)
 end
 
 function Prototype:ApplyBaseSkin()
+    local width, height = self:GetSize()
     self.IconBorder:SetTexture([[Interface\Buttons\UI-ActionButton-Border]])
-    self.IconBorder:SetSize(70, 70)
+    self.IconBorder:SetTexCoord(0.218, 0.718 , 0.234, 0.781)
+    self.IconBorder:SetSize(width, height)
     self.IconBorder:SetBlendMode("ADD")
 
     self.BorderFrame:SetTexture([[Interface\Buttons\UI-ActionButton-Border]])
+    self.BorderFrame:SetTexCoord(0.218, 0.718 , 0.234, 0.781)
     self.BorderFrame:SetPoint("CENTER")
     self.BorderFrame:SetBlendMode("ADD")
     self.BorderFrame:SetAlpha(0.8)
-    self.BorderFrame:SetSize(70, 70)
+    self.BorderFrame:SetSize(width, height)
 
     self.ItemLevel:SetPoint("TOP")
 
